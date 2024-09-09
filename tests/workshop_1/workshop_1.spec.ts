7e156c3bde183cb75b3e1d5c34e2994e116becc4
isnendyankp <isnendyankp@gmail.com>
2024-09-09 12:47:18 +0700

    Add: Testing with interact page with input first name and last name

--- tests/workshop_1/workshop_1.spec.ts
+++ tests/workshop_1/workshop_1.spec.ts
@@ -1,9 +1,21 @@
 import {test} from '@playwright/test';
+import { on } from 'events';
 
 
 // basic navigation test
+// can test with npx playwright test --ui
 test('basic navigation test', async ({ page }) => {
     await page.goto('https://gitlab.com/');
     await page.waitForTimeout(3000);
     await page.reload();
+});
+
+
+// test interaction with the page
+// test with npx playwright test --project chromium --headed for visual testing
+test.only('test interaction with the page', async ({ page }) => {
+    await page.goto('https://gitlab.com/');    
+    await page.locator('#be-navigation-desktop').getByRole('link', {name: 'Get free trial'}).click();
+    await page.locator('[data-testid="new-user-first-name-field"]').fill('John');
+    await page.locator('[data-testid="new-user-last-name-field"]').fill('Doe');
 });
