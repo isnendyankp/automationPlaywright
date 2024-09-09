aed57493a7c129b3e327f39a333511a61721e017
isnendyankp <isnendyankp@gmail.com>
2024-09-09 12:51:13 +0700

    Refactor test files and add basic navigation and interaction tests

--- tests/workshop_1/workshop_1.spec.ts
+++ tests/workshop_1/workshop_1.spec.ts
@@ -13,9 +13,14 @@ test('basic navigation test', async ({ page }) => {
 
 // test interaction with the page
 // test with npx playwright test --project chromium --headed for visual testing
+// Note:
+// locator digunakan untuk mencari elemen pada halaman
 test.only('test interaction with the page', async ({ page }) => {
-    await page.goto('https://gitlab.com/');    
+    await page.goto('https://gitlab.com/');
+    // kode ini akan mencari elemen dengan role link dan nama Get free trial lalu melakukan klik
     await page.locator('#be-navigation-desktop').getByRole('link', {name: 'Get free trial'}).click();
+    // kode ini mengetest input first name dan last name pada halaman
+    // data-testid digunakan untuk memberikan nama pada elemen pada kasus ini first name dan last name menggunakan for 
     await page.locator('[data-testid="new-user-first-name-field"]').fill('John');
     await page.locator('[data-testid="new-user-last-name-field"]').fill('Doe');
 });
