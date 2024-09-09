dfc6dbcf50f6463f12f8b1a62b05ee25f16541ca
isnendyankp <isnendyankp@gmail.com>
2024-09-09 14:45:34 +0700

    Add test for link sign at homepage

--- tests/workshop_1/workshop_1.spec.ts
+++ tests/workshop_1/workshop_1.spec.ts
@@ -4,7 +4,7 @@ import { on } from 'events';
 
 // basic navigation test
 // can test with npx playwright test --ui
-test('basic navigation test', async ({ page }) => {
+test.skip('basic navigation test', async ({ page }) => {
     await page.goto('https://gitlab.com/');
     await page.waitForTimeout(3000);
     await page.reload();
@@ -15,7 +15,7 @@ test('basic navigation test', async ({ page }) => {
 // test with npx playwright test --project chromium --headed for visual testing
 // Note:
 // locator digunakan untuk mencari elemen pada halaman
-test.only('test interaction with the page', async ({ page }) => {
+test.skip('test interaction with the page', async ({ page }) => {
     await page.goto('https://gitlab.com/');
     // kode ini akan mencari elemen dengan role link dan nama Get free trial lalu melakukan klik
     await page.locator('#be-navigation-desktop').getByRole('link', {name: 'Get free trial'}).click();
@@ -23,4 +23,12 @@ test.only('test interaction with the page', async ({ page }) => {
     // data-testid digunakan untuk memberikan nama pada elemen pada kasus ini first name dan last name menggunakan for 
     await page.locator('[data-testid="new-user-first-name-field"]').fill('John');
     await page.locator('[data-testid="new-user-last-name-field"]').fill('Doe');
+});
+
+
+// test interaction with the page by click button sign in at main menu dropdown
+// test with npx playwright test --project chromium --headed for visual testing
+test.only('test interaction with the page by click button sign in at main menu dropdown', async ({ page }) => {
+    await page.goto('https://gitlab.com/');
+    await page.getByRole('link', {name: 'Sign in'}).click();
 });
